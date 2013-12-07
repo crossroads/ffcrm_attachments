e8b7d41ac12b2c235ae7b12babcf80b032f8a1f1
Simon Pang <hksimon@gmail.com>
2013-12-07 12:14:17 +0800

    Added Attachment polymorphic file class

--- lib/ffcrm_attachments/engine.rb
+++ lib/ffcrm_attachments/engine.rb
@@ -1,4 +1,22 @@
 module FfcrmAttachments
   class Engine < ::Rails::Engine
+
+ 	config.to_prepare do
+
+		require "polymorphic/attachment"
+
+ 		ActiveSupport.on_load(:fat_free_crm_attachment) do
+
+ 			ENTITIES.each do |entity| 
+	 			entity.constantize.class_eval do
+
+	 				has_many :attachments
+
+	 			end
+ 			end
+
+ 		end
+
+ 	end
   end
 end
